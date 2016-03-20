f22cd5a191071cf06076da940fcbf61355f8bc97
chrys <chrys87@web.de>
2016-03-20 23:37:39 +0100

    create dict with filenames

--- pybottle/storm_games.py
+++ pybottle/storm_games.py
@@ -8,13 +8,16 @@ from os.path import isfile, join
 import pygame
 import time
 
+SoundFolder = 'sounds'
+
 def initialize_gui(gameTitle):
     # start pygame
     pygame.init()
     # start the display (required by the event loop)
     pygame.display.set_mode((320, 200))
     pygame.display.set_caption(gameTitle)
-    # Load sounds from the sound directory
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    # Load sounds from the sound directory and creates a dict like that {'bottle': 'bottle.ogg'}
+    soundFiles = {f.split('.')[0]:f for f in listdir(SoundFolder) if isfile(join(SoundFolder, f))}
+
 
 
